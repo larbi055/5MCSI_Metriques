48237881ecad7690abe96c2332956e2ed943d038
larbi055 <larbihamani05@gmail.com>
2025-12-03 10:16:48 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,7 +8,7 @@ import sqlite3
 app = Flask(__name__)                                                                                                                  
                                                                                                                                        
 @app.route('/')
-def hello_world():
+def hello_world(): 
     return render_template('hello.html') 
   
 if __name__ == "__main__":
