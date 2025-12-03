8df3a5cd97bc36a696aa3a0b6ed51619807d62f5
larbi055 <larbihamani05@gmail.com>
2025-12-03 11:27:19 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -52,6 +52,10 @@ def commits_data():
             results.append(minute)
 
     return jsonify(results=results)
+  
+  @app.route("/commits/")
+def commits():
+    return render_template("commits.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
